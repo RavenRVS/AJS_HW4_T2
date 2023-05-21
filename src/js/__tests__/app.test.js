3aa49f13b656fef32ea3e77a411010767ba7ffef
Vitaly Remnev <remnevvs@mail.ru>
2023-05-21 19:22:28 +0300

    fix test

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -10,17 +10,6 @@ test.each([
     { name: 'лучник', health: 80 },
     { name: 'мечник', health: 10 },
   ]],
-  ['list2 with two identical values', [
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'вор', health: 80 },
-  ], [
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'вор', health: 80 },
-    { name: 'мечник', health: 10 },
-  ]],
 ])(
   ('should sort the %s by the value of the "health" field'),
   (level, persList, expected) => {
